9f5255bcc50680bae7cdb4c302c7e13064bd301b
Rojalpraz <prazrojal@gmail.com>
2025-12-13 09:47:24 +0545

    FIX

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   plugins: [react()
     , tailwindcss()
   ],
